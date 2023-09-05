840ca733d0d43041a477945aed2043dfbe15c500
Nadwey <nadweyyy@gmail.com>
2023-09-05 19:34:45 +0200

    Add Toggle FPS Counter Button

---
+++ Source/PianofallUE/Public/PFUE_Utils.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PFUE_Utils.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PIANOFALLUE_API UPFUE_Utils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
